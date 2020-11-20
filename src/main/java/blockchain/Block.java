65f2628bb6c853cdfbf39d7e8cadc7e979cc9378
juengeja <jannis.juengert@sepio.de>
2020-11-20 11:46:23 +0100

    Latest Update

--- src/main/java/blockchain/Block.java
+++ src/main/java/blockchain/Block.java
@@ -1,4 +1,5 @@
 package blockchain;
+
 import java.util.Date;
 
 public class Block {
@@ -10,30 +11,35 @@ public class Block {
     private int nonce = 0;
 
     //Block Constructor.
-    public Block(String data,String previousHash ) {
+    public Block(String data, String previousHash) {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash(); // Wichtig: Muss nach den anderen Werten gesetzt werden!
     }
+
+    // Hash-Erzeugung
     public String calculateHash() {
+
         return Hashing.hashSha256(previousHash + timeStamp + data + nonce);
     }
-    public void mineBlock(int difficulty){
+
+    // Header "minen"
+    public void mineBlock(int difficulty) {
         boolean mined = false;
         String ideal = String.format("%0" + difficulty + "d", 0);
         String part;
         //System.out.println(ideal);
         System.out.println("Mining new Block...");
-        while(!mined){
-            nonce ++;
+        while (!mined) {
+            nonce++;
             //System.out.println(nonce);
             hash = calculateHash();
             //System.out.println(hash);
             part = hash.substring(0, difficulty);
             //System.out.println(part);
 
-            if(part.equals(ideal)){ // Vergleichender Operator "==" nicht ausreichend
+            if (part.equals(ideal)) { // Vergleichender Operator "==" nicht ausreichend
                 mined = true;
 
             }
